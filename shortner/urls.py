6a31fc2a07c78c4296234bf0fab70dc85ac0c00c
ghanteyyy <ghanteyyy@gmail.com>
2026-02-02 08:34:36 +0545

    Added url endpoints

--- shortner/urls.py
+++ shortner/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('api/url/shorten/', views.Shorten_URL.as_view(), name="shorten_url")
+    path('api/url/shorten/', views.Shorten_URL.as_view(), name="shorten_url"),
 ]
 
